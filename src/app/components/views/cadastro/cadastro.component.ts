e1ba366e5ab16386c67bfba9357e5f35394f0c4c
Maria Luiza B <maria.barbosa@opus-software.com.br>
2023-07-19 21:48:28 -0300

    cadastro with steps 1 and 2

--- src/app/components/views/cadastro/cadastro.component.ts
+++ src/app/components/views/cadastro/cadastro.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormBuilder, FormGroup} from '@angular/forms';
 import {NgIf} from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatSelectModule} from '@angular/material/select';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,14 +14,36 @@ import { Router } from '@angular/router';
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.scss'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, NgIf, MatButtonModule, MatIconModule],
+  imports: [
+    MatFormFieldModule, 
+    MatInputModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    NgIf, 
+    MatButtonModule, 
+    MatStepperModule,
+    MatSelectModule,
+    MatIconModule
+  ],
 })
 export class CadastroComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private _formBuilder: FormBuilder) {}
   hide = true;
   email = new FormControl('', [Validators.required, Validators.email]);
   senha = new FormControl('', [Validators.required]);
+  divVisivel1: boolean = false;
+  divVisivel2: boolean = false;
+  divVisivel3: boolean = false;
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  isLinear = false;
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
@@ -35,6 +59,18 @@ export class CadastroComponent {
 
   goToHomePage() {
     this.router.navigate(['/']);
+  }
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+
+  mostrarDiv(event: any) {
+    const selectedOptions = event.value;
+  
+    this.divVisivel1 = selectedOptions && selectedOptions.lenght >0 ;
+    this.divVisivel2 = selectedOptions && selectedOptions[1] ;
+    this.divVisivel3 = selectedOptions && selectedOptions[0] && selectedOptions[1];
   }
 
 }
